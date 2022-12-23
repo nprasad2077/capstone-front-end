f4e9e14cd2cb28944f575ac720797c77d219b835
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-23 16:04:04 -0600

    potentiall dangerous asteroid table map

--- src/components/Asteroids/Asteroids.js
+++ src/components/Asteroids/Asteroids.js
@@ -4,12 +4,12 @@ import { useState } from 'react'
 const EONET = ({neo}) => {
     const [asteroidList, setAsteroidList] = useState([])
     const neoReturn = neo && neo['near_earth_objects']['2022-12-23']
+    console.log(neo && neoReturn[0].close_approach_data[0].close_approach_date_full)
+    console.log(neo && neoReturn[0].estimated_diameter.miles.estimated_diameter_max)
+    console.log(neo && neoReturn[0].is_potentially_hazardous_asteroid)
 
-    // console.log(eonet && eonetReturn.map(asteroid => asteroid.name));
-    const rocksMap = neo && neoReturn.map(asteroid => {
-        return <tr><td>{asteroid.name}</td></tr>})
-    console.log(rocksMap);
-
+    const rocksMap = neo && neoReturn.map((asteroid, index) => {
+        return <tr key={index}><td>{asteroid.name}</td><td>{asteroid.estimated_diameter.miles.estimated_diameter_max} miles</td><td>{asteroid.is_potentially_hazardous_asteroid ? '☢️' : '❎'}</td></tr>})
 
 
 
@@ -19,9 +19,10 @@ const EONET = ({neo}) => {
         <table>
             <thead>
                 <tr>
-                    <th>name</th>
-                    <th>size</th>
-                    <th>date</th>
+                    <th>Asteroid Name</th>
+                    <th>Size {'(diameter)'} </th>
+                    <th>Potential Hazardous Asteroid?</th>
+                    <th>Approach Date</th>
                 </tr>
             </thead>
             <tbody>
