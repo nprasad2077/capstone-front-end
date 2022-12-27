cfd05adbd9c18b4599101d40e6738c07af4767b2
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-27 10:19:59 -0600

    EPIC image gallery works!

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -17,10 +17,23 @@ const images = [
   },
 ];
 
-const SlideShow = () => {
+const SlideShow = ({epic}) => {
+  const slideshowEpic = epic && epic.map((images) => 'https://epic.gsfc.nasa.gov/archive/natural/2022/12/25/png/'+ images.image + '.png')
+  const slideshowEpicJpg = epic && epic.map((images) => 'https://epic.gsfc.nasa.gov/archive/natural/2022/12/25/jpg/'+ images.image + '.jpg')
+  const imagesARR = []
+
+  const imagesCreate = epic && slideshowEpic.map((images) => imagesARR.push({original: images, thumbnail: images}))
+
+  console.log(slideshowEpicJpg)
+  console.log(imagesARR)
+  // console.log(images);
+
+
+
+
   return (
     <div class='border border-sky-500 content-center'>
-      <ImageGallery items={images} />
+      <ImageGallery items={imagesARR} />
     </div>
   )
 }
