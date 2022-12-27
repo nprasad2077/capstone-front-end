0822eeef583ab5278df36e1e79a29b5cf089a10b
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-26 22:16:29 -0600

    react slide

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import './SlideShow.css'
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+  {
+    original: 'https://epic.gsfc.nasa.gov/archive/natural/2022/12/25/png/epic_1b_20221225010437.png',
+    thumbnail: 'https://epic.gsfc.nasa.gov/archive/natural/2022/12/25/png/epic_1b_20221225010437.png',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 const SlideShow = () => {
   return (
-    <div class='border border-sky-500 content-center'>SlideShow</div>
+    <div class='border border-sky-500 content-center'>
+      <ImageGallery items={images} />
+    </div>
   )
 }
 
