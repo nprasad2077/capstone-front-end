9fe54e0b2b69a7217cbeb7903bc66a3e6be9f53e
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-29 15:53:34 -0600

    state used for point size

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import {Cartesian3} from 'cesium'
 import { Color } from "cesium";
-import { Viewer, Entity } from "resium";
+import { Viewer, Entity, PointGraphics } from "resium";
+import { useState } from 'react';
 
 const position = Cartesian3.fromDegrees( -95.36200, 29.75060, 0)
 const pointGraphics = { pixelSize: 10 }
 
+
 const Globe = () => {
+  const [pixelNum, setPixelNum] = useState(20)
+
+
   return (
     <div class='text-center'>
       <h2 class='text-3xl'>GLOBE</h2>
 
       <Viewer>
-        <Entity position={position} point={pointGraphics} />
+        <Entity position={position}>
+          <PointGraphics pixelSize={pixelNum} />
+        </Entity>
       </Viewer>
     </div>
   )
