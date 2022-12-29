912c2f19a5c3cfe1db7f0b8754b90003539f5619
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-29 14:14:25 -0600

    GLOBE COMPONENT WORKS

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Cartesian3} from 'cesium'
-import {Viewer} from 'resium'
+import { Color } from "cesium";
+import { Viewer, Entity } from "resium";
 
 const Globe = () => {
   return (
     <div class='text-center'>
       <h2 class='text-3xl'>GLOBE</h2>
+
+      <Viewer full />
 
 
     </div>
