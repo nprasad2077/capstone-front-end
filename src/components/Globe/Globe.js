05147d5e005b60987b37f2633f81894bd6790cdc
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-29 15:58:25 -0600

    JSX description on globe point

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Cartesian3} from 'cesium'
 import { Color } from "cesium";
-import { Viewer, Entity, PointGraphics } from "resium";
+import { Viewer, Entity, PointGraphics, EntityDescription } from "resium";
 import { useState } from 'react';
 
 const position = Cartesian3.fromDegrees( -95.36200, 29.75060, 0)
@@ -15,10 +15,13 @@ const Globe = () => {
   return (
     <div class='text-center'>
       <h2 class='text-3xl'>GLOBE</h2>
-
       <Viewer>
-        <Entity position={position} name='Houston' description='Hello, world!'>
+        <Entity position={position} name='Houston'>
           <PointGraphics pixelSize={pixelNum} />
+          <EntityDescription>
+            <h1>Hello, world!</h1>
+            <p>JSX works here!</p>
+          </EntityDescription>
         </Entity>
       </Viewer>
     </div>
