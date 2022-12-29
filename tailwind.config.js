9bd1d20620a4579e35a0940db6cc48d128e30373
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-29 12:48:52 -0600

    react flowbite :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: ["./src/**/*.{html,js}",
   "./public/**/*.{html,js}",
-  "./node_modules/flowbite/**/*.js"],
+  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+],
   theme: {
     extend: {},
   },
