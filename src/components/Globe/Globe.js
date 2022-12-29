b1fd04013494368d30e5ae7653605b3462dcd3dd
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-29 15:40:29 -0600

    globe center houston 🌍🌐🌎

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -3,14 +3,17 @@ import {Cartesian3} from 'cesium'
 import { Color } from "cesium";
 import { Viewer, Entity } from "resium";
 
+const position = Cartesian3.fromDegrees( -95.36200, 29.75060, 0)
+const pointGraphics = { pixelSize: 10 }
+
 const Globe = () => {
   return (
     <div class='text-center'>
       <h2 class='text-3xl'>GLOBE</h2>
 
-      <Viewer />
-
-
+      <Viewer>
+        <Entity position={position} point={pointGraphics} />
+      </Viewer>
     </div>
   )
 }
