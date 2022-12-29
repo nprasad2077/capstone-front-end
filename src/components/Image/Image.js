b8ae0fae2e257a84bdb1b2a23fc5928d04a54f80
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-28 21:55:09 -0600

    static search

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 const Image = () => {
+  const [search, setSearch] = useState([])
+  const [titles, setTitles] = useState([])
+
+  const getSearch = async () => {
+    const response = await axios({
+      method: 'get',
+      url: 'https://images-api.nasa.gov/search?q=moon'
+    })
+    .then(res => setSearch(res.data))
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getSearch()
+  }, [])
+
+  console.log(search);
+
+  const searchDetails = search.collection && search.collection.items
+
+  console.log(searchDetails);
+
+  const searchMapped = search.collection && searchDetails.map((search) => <p>{search.data[0].title}</p>)
+
+  console.log(searchMapped);
+
+
+
   return (
-    <div class='border border-sky-500 text-center'>
-      <h2>NASA Image and Video Library</h2>
+    <div class='border border-sky-500 text-center flex-col'>
+
+      <h2 class='text-center text-2xl subpixel-antialiased font-bold'>NASA Image and Video Library</h2>
+      <div class='flex-col mt-10'>
+        {searchMapped}
+      </div>
 
     </div>
   )
