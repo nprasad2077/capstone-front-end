3546f2a69ed7133598521c3a641d64de38315a2e
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-30 19:25:56 -0600

    persrovr parse works

--- src/components/Perseverance/Perseverance.js
+++ src/components/Perseverance/Perseverance.js
@@ -6,10 +6,24 @@ const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?
 
 const Perseverance = ({persRover}) => {
   console.log(persRover.photos);
-  const persRoverMapped = persRover.photos && persRover.photos.map((rover) => rover.camera.name)
-  // const [medaSky, setMedaSky] = useState([])
-  const medaSkyPics = []
-console.log(persRoverMapped);
+  const navcamLeft = []
+  const navcamLeftDate = []
+
+  function parsePersRover(rov) {
+    if (rov.camera.name === 'NAVCAM_LEFT') {
+      navcamLeft.push(rov.img_src)
+      navcamLeftDate.push(rov.earth_date)
+    }
+
+  }
+
+  const persRoverMapped = persRover.photos && persRover.photos.map((rover) => parsePersRover(rover))
+
+  console.log(persRoverMapped);
+  console.log(navcamLeft, navcamLeftDate);
+
+
+
 
   return (
     <div>
