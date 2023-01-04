599e3447e8e48c6ad1f2d3e02d134a6082441614
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-04 16:16:34 -0600

    find astronaut works on detail

--- src/components/AstroDetail/AstroDetail.js
+++ src/components/AstroDetail/AstroDetail.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const AstroDetail = ({astro}) => {
+  const {id} = useParams()
+  const astronaut = astro
+  const found = astro.find(element => element.id == id)
 
-  console.log(astro);
+  console.log(astro, id)
+  console.log(found);
+
   return (
-    <div>AstroDetail</div>
+    <div>
+        <a href="#" class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src='' alt=""/>
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Blah</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </a>
+    </div>
   )
 }
 
