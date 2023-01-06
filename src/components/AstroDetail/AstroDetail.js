d7cf99da89b3f424c848a477bdb57567c8722dac
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-06 06:35:11 -0600

    astronaut details aligned

--- src/components/AstroDetail/AstroDetail.js
+++ src/components/AstroDetail/AstroDetail.js
@@ -11,7 +11,7 @@ const AstroDetail = ({astro}) => {
 
 
   return (
-    <div class='flex-col items-center justify-center'>
+    <div class='flex-col items-center justify-center'><div class='flex items-center justify-center'>
         <a class="flex flex-col items-center bg-gray-800 border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-700 border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
             <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={astronaut.photo_url} alt=""/>
             <div class="flex flex-col justify-between p-4 leading-normal">
@@ -19,7 +19,7 @@ const AstroDetail = ({astro}) => {
                 <p class="mb-3 font-normal text-gray-400 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.{planets}</p>
             </div>
         </a>
-    </div>
+    </div></div>
   )
 }
 
