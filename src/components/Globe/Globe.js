fb4f8aa3ebdc5e30b64d714119bfe1e1c7573aaf
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-06 04:57:45 -0600

    clean up globe

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -45,18 +45,6 @@ const Globe = ({lat, long, setLat, setLong}) => {
       <Viewer fullscreenButton homeButton={true} terrtainProvider={terrtainProvider}>
         <CameraFlyTo duration={5} destination={position} once={'once'} />
       </Viewer>
-      {/* <div class='mt-4'>
-        <label for="small-input" class="block mb-2 text-sm font-medium text-white">latitude</label>
-        <input onChange={latpos} type="text" id="small-input" class="p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-        
-      </div>
-      <div class='mt-2'>
-        <label for="small-input" class="block mb-2 text-sm font-medium text-white">longitude</label>
-        <input onChange={longpos} type="text" id="small-input" class="p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-      </div>
-      <div>
-      <button onClick={positionChange} type="button " class="text-white mt-4 bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Set</button>
-      </div> */}
     </div>
   )
 }
