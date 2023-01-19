03e9e58bd812787a8b16bea9f924a2cb0227b987
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-18 22:02:41 -0600

    margin tail and bg EPIC

--- src/components/Tail/Tail.js
+++ src/components/Tail/Tail.js
@@ -3,7 +3,7 @@ import { Footer } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 
 const Tail = () => {
-  return (<div>
+  return (<div class='mt-10'>
     <footer class="p-4 sm:p-6 bg-gray-800">
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
