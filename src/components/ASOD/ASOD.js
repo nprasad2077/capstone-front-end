77ae4487f8b46e3dea312651650434f4063116d6
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-18 21:55:51 -0600

    updated buttons and ASOD margins

--- src/components/ASOD/ASOD.js
+++ src/components/ASOD/ASOD.js
@@ -3,11 +3,11 @@ import React from 'react'
 const ASOD = ({asod}) => {
 
   return (
-    <div class='mt-5 border-2 border-slate-300 content-center flex-col p-5 mx-5 bg-slate-800'>
+    <div class='mt-5 border-2 border-slate-300 content-center flex-col p-4 mx-5 bg-slate-800'>
       <h2 class='text-center text-sm'>Astronomy Picture of the Day</h2>
-      <h3 class='text-center text-2xl subpixel-antialiased font-bold'>{asod.title}</h3>
-      <img src={asod.hdurl} class='self-center mt-6'></img>
-      <p class='mt-10 border-2 border-double rounded p-4 '>Description: {asod.explanation}</p>
+      <h3 class='text-center text-2xl subpixel-antialiased font-bold mt-4'>{asod.title}</h3>
+      <img src={asod.hdurl} class='self-center mt-6 p-1 w-full'></img>
+      <p class='mt-8 p-6 '>Description: {asod.explanation}</p>
     </div>
   )
 }
