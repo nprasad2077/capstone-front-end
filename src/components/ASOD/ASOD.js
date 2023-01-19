f786cc5c337561f6f330acd972f316fc0767c57d
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-18 21:45:19 -0600

    AI css changes on button 🫡

--- src/components/ASOD/ASOD.js
+++ src/components/ASOD/ASOD.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ASOD = ({asod}) => {
 
   return (
-    <div class='mt-5 border-4 border-slate-300 content-center flex-col p-5 mx-5 bg-slate-800'>
+    <div class='mt-5 border-2 border-slate-300 content-center flex-col p-5 mx-5 bg-slate-800'>
       <h2 class='text-center text-sm'>Astronomy Picture of the Day</h2>
       <h3 class='text-center text-2xl subpixel-antialiased font-bold'>{asod.title}</h3>
       <img src={asod.hdurl} class='self-center mt-6'></img>
