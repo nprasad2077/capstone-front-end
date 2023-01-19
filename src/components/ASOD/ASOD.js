13471fbfcbaf89ba595c487c281cd3bb5324d0fc
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-18 21:29:18 -0600

    updated ASOD background and text border

--- src/components/ASOD/ASOD.js
+++ src/components/ASOD/ASOD.js
@@ -3,11 +3,11 @@ import React from 'react'
 const ASOD = ({asod}) => {
 
   return (
-    <div class='mt-2 border-4 border-slate-900 content-center flex-col p-5 mr-5 '>
+    <div class='mt-5 border-4 border-slate-900 content-center flex-col p-5 mx-5 bg-slate-800'>
       <h2 class='text-center text-sm'>Astronomy Picture of the Day</h2>
       <h3 class='text-center text-2xl subpixel-antialiased font-bold'>{asod.title}</h3>
       <img src={asod.hdurl} class='self-center mt-6'></img>
-      <p class='mt-2'>Description: {asod.explanation}</p>
+      <p class='mt-10 border-2 border-double rounded p-4 '>Description: {asod.explanation}</p>
     </div>
   )
 }
