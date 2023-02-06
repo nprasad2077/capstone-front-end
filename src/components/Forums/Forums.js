dbf780240e250836be3fa0ab503739a0f186835e
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-06 14:54:10 -0600

    forum post button created

--- src/components/Forums/Forums.js
+++ src/components/Forums/Forums.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'flowbite-react';
+import { Card, Button } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 
 const Forums = ({forums, astro}) => {
@@ -14,10 +14,20 @@ const Forums = ({forums, astro}) => {
   </Card>
 </ div ></Link>)
 
+  const createPost = () => {
+    console.log('create post');
+  }
+
+
 
   return (
     <div class='flex-col items-center justify-center'>
       <div class='flex items-center justify-center'>{forumsMap}</div>
+      <div class='flex items-center justify-center'>
+        <Button color="success" onClick={createPost}>
+          Create Forum Post
+        </Button>
+      </div>
     </div>
   )
 }
