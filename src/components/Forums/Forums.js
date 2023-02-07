f577c093e8b4927ba82e010985e8ce12d18df2fa
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-06 19:19:27 -0600

    forum post button works

--- src/components/Forums/Forums.js
+++ src/components/Forums/Forums.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Button } from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Forums = ({forums, astro}) => {
+  const [forumData, setForumData] = useState({
+    astronaut_id: 1,
+    title: 'Test Forum from React State',
+    photo: 'https://www.nasa.gov/sites/default/files/thumbnails/image/iss042e012000.jpg',
+    preview_url: 'testing'
+  })
+  const postURL = 'https://polar-everglades-56224.herokuapp.com/forums/'
 
   console.log(forums, astro);
+  console.log(forumData);
 
   const forumsMap = forums.map((map) => <Link to={'/forums/' + map.id}><div class='w-96'>
   <Card>
@@ -14,8 +23,14 @@ const Forums = ({forums, astro}) => {
   </Card>
 </ div ></Link>)
 
-  const createPost = () => {
-    console.log('create post');
+  const createPost = async () => {
+    const response = await axios.post(postURL, forumData)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
 
