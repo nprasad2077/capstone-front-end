f9e6714b0a76c018831ade1f74c1575a51c62dc3
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-06 20:12:10 -0600

    put forums works!

--- src/components/ForumsDetail/ForumsDetail.js
+++ src/components/ForumsDetail/ForumsDetail.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Card } from 'flowbite-react'
+import { Button, Card } from 'flowbite-react'
+import ReactModal from 'react-modal';
+import { useState } from 'react';
+import axios from 'axios';
 
 const ForumsDetail = ({forums, astro}) => {
-
   const {id} = useParams()
-  console.log(id);
-
+  const [show, setShow] = useState(false)
   console.log(forums, astro);
 
   const findForum = forums.find((forum) => forum.id === parseInt(id))
-  console.log(findForum.title);
+  console.log(findForum);
+  const [postUpdate, SetPostUpdate] = useState({
+    astronaut_id: findForum.astronaut_id,
+    title: findForum.title,
+    photo: findForum.photo,
+    preview_url: findForum.preview_url
+  })
 
   const forumsMapDetail = forums.map((map) => <div class='w-96 self-center'>
   <Card>
@@ -27,12 +34,30 @@ const commentsMap = forums.map((map) => <div class='w-96 mt-6'><Card>
 <p className="font-normal text-gray-400">{map.preview_url}</p>
 </Card></div>)
 
+const updateForumPost = () => {
+  const response = axios.put('https://polar-everglades-56224.herokuapp.com/forums/' + findForum.id, postUpdate)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+
+
+const showModal = () => { setShow(true) }
+const hideModal = () => { setShow(false) }
+
+const updateStack = () => {
+  updateForumPost()
+  hideModal()
+}
 
 
 
   return (
-    <div class='flex-col items-center justify-center'>
-      <div class='w-96 self-center'>
+    <div class='flex-col items-center justify-center content-center'>
+      <div class='w-96 self-center justify-center items-center flex'>
         <Card>
           <h5 className="text-2xl font-bold tracking-tight text-black">
               {findForum.title}
@@ -41,7 +66,22 @@ const commentsMap = forums.map((map) => <div class='w-96 mt-6'><Card>
           </p>
           <img  class='' src={findForum.photo} alt='forum post'></img>
         </Card>
+        <Button onClick={showModal}>
+          Update Forum Post
+        </Button>
       </ div >
+      <div>
+        <ReactModal isOpen={show}>
+          <h1>Update Forum Post</h1>
+          <Button onClick={hideModal}>X</Button>
+          <Button onClick={updateStack}>Update</Button>
+          <div>
+            <input type='text' value={postUpdate.title} onChange={(e) => SetPostUpdate({...postUpdate, title: e.target.value})}></input>
+            <input type='text' value={postUpdate.photo} onChange={(e) => SetPostUpdate({...postUpdate, photo: e.target.value})}></input>
+            <input type='text' value={postUpdate.preview_url} onChange={(e) => SetPostUpdate({...postUpdate, preview_url: e.target.value})}></input>
+          </div>
+        </ReactModal>
+      </div>
     </div>
   )
 }
