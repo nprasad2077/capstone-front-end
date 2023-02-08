c736316baaf961541909ce3a679e3c5f8c78132c
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-07 21:40:49 -0600

    list item astro details and padding/margins added

--- src/components/AstroDetail/AstroDetail.js
+++ src/components/AstroDetail/AstroDetail.js
@@ -15,7 +15,7 @@ const AstroDetail = ({astro}) => {
   const [photoUrl, setPhotoUrl] = React.useState('');
   const [planetsArr, setPlanetsArr] = React.useState(['test', 'test2']);
   const astronaut = astro && astro.find(element => element.id == id)
-  const planets = astro && astronaut.planets.map(planet => <p> {planet} </p>)
+  const planets = astro && astronaut.planets.map(planet => <li> {planet} </li>)
 
   const goBack = () => {navigate(-1)}
 
@@ -45,13 +45,13 @@ const AstroDetail = ({astro}) => {
 
   return (
     <div class='flex-col items-center justify-center'>
-    <h1 class='text-4xl font-bold text-center text-white mb-10'>Astronaut Details</h1>
-    <div class='flex items-center justify-center'>
+    <h1 class='text-4xl font-bold text-center text-white mb-10 mt-2'>Astronaut Details</h1>
+    <div class='flex items-center justify-center p-6'>
       <div class="flex flex-col items-center bg-gray-800 border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-700 border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" >
-          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={astronaut.photo_url} alt=""/>
+          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg ml-4 p-1/2" src={astronaut.photo_url} alt=""/>
           <div class="flex flex-col justify-between p-4 leading-normal">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">{astronaut.name}</h5>
-              <p class="mb-3 font-normal text-gray-400 dark:text-gray-400">Here are the biggest space destinations this year: {planets}</p>
+              <h5 class=" text-2xl font-bold tracking-tight text-white dark:text-white mb-6">{astronaut.name}</h5>
+              <p class="mb-3 font-normal text-gray-400 dark:text-gray-400">Here are the biggest space destinations this year: <div class='mt-4 ml-6'>{planets}</div></p>
           </div>
       </div>
     </div>
