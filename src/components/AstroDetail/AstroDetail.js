4438c780e0d42e1778d69cedceb81bb63c227204
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-07 21:23:16 -0600

    update button and delete centered on astro-detail

--- src/components/AstroDetail/AstroDetail.js
+++ src/components/AstroDetail/AstroDetail.js
@@ -45,22 +45,23 @@ const AstroDetail = ({astro}) => {
 
   return (
     <div class='flex-col items-center justify-center'>
-    <h1 class='text-4xl font-bold text-center text-white mb-4'>Astronaut Details</h1>
+    <h1 class='text-4xl font-bold text-center text-white mb-10'>Astronaut Details</h1>
     <div class='flex items-center justify-center'>
-      <a class="flex flex-col items-center bg-gray-800 border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-700 border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <div class="flex flex-col items-center bg-gray-800 border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-700 border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" >
           <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={astronaut.photo_url} alt=""/>
           <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">{astronaut.name}</h5>
               <p class="mb-3 font-normal text-gray-400 dark:text-gray-400">Here are the biggest space destinations this year: {planets}</p>
           </div>
-      </a>
+      </div>
     </div>
-
-    <div>
-      <Button onClick={showModal}>Update information</Button>
-    </div>
-    <div>
-      <Button color='failure' onClick={deleteInfo}>Delete Astronaut</Button>
+    <div class='flex flex-col items-center justify-center mt-40'>
+      <div>
+        <Button onClick={showModal}>Update information</Button>
+      </div>
+      <div class='mt-4'>
+        <Button color='failure' onClick={deleteInfo}>Delete Astronaut</Button>
+      </div>
     </div>
 
     <ReactModal isOpen={show} shouldCloseOnOverlayClick={true} shouldCloseOnEsc={true} onRequestClose={hideModal}>
