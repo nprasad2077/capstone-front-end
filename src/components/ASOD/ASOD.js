b82cc9f6954b0f3edde6384dbf845002f488d824
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-08 22:35:22 -0600

    better button css

--- src/components/ASOD/ASOD.js
+++ src/components/ASOD/ASOD.js
@@ -1,7 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react'
 import axios from 'axios';
 import { useEffect } from 'react';
 import { Button } from 'flowbite-react';
+import './asod.css'
 
 const ASOD = ({asod, mongoPut}) => {
 
@@ -21,9 +24,13 @@ const ASOD = ({asod, mongoPut}) => {
     <div class='mt-5 border-2 border-slate-300 content-center flex-col p-4 mx-5 bg-slate-800'>
       <h2 class='text-center text-sm'>Astronomy Picture of the Day</h2>
       <h3 class='text-center text-2xl subpixel-antialiased font-bold mt-4'>{asod.title}</h3>
-      <img src={asod.hdurl} class='self-center mt-6 p-1 w-full'></img>
+      <img src={asod.hdurl} alt='NASA ASOD' class='self-center mt-6 p-1 w-full'></img>
       <p class='mt-8 p-6 '>Description: {asod.explanation}</p>
-      <Button onClick={putASOD}>❤️</Button>
+      <div class='flex flex-col justify-center items-center'>
+        <a  onClick={putASOD} class="myButton">❤️</a>
+        <p class='mt-2'>Save to Favorites</p>
+      </div>
+      
     </div>
   )
 }
