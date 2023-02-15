bb9dc7dbc0493497394fd644fe24afd960ccfb55
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-14 18:24:53 -0600

    install HMR, pre-craco config change

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -8,7 +8,7 @@ const Globe = ({lat, long, setLat, setLong}) => {
   const point = {pixelSize: 10}
 
   const position = Cartesian3.fromDegrees( long, lat, 1000)
-  const positionTwo = Cartesian3.fromDegrees( -74, 40, 100)
+  const positionTwo = Cartesian3.fromDegrees( -74, 40, 1000)
 
 
   return (
