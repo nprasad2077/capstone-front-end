725eaf2250e60cee8654f35583988e2dd7602b4e
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-14 18:08:59 -0600

    globe is working

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -1,44 +1,20 @@
 import React from 'react'
-import {Cartesian3, createWorldTerrain} from 'cesium'
-import { Color } from "cesium";
-import { Viewer, Entity, PointGraphics, EntityDescription, CameraFlyTo, ImageryLayer} from "resium";
-import { useState } from 'react';
-import { ArcGisMapServerImageryProvider } from 'cesium';
+import {Cartesian3} from 'cesium'
+import { Viewer, Entity} from "resium";
+
 
 
 const Globe = ({lat, long, setLat, setLong}) => {
-  const [pixelNum, setPixelNum] = useState(20)
-  const [latChange, setLatChange] = useState(0)
-  const [longChange, setLongChange] = useState(0)
+  const point = {pixelSize: 10}
 
   const position = Cartesian3.fromDegrees( long, lat, 1000)
-
-  const terrtainProvider = createWorldTerrain({
-        requestWaterMask : true,
-        requestVertexNormals : true
-  })
-
-  const latpos = (e) => {
-    setLatChange(parseInt(e.target.value))
-  }
-
-  const longpos = (e) => {
-    setLongChange(parseInt(e.target.value))
-  }
-
-  const positionChange = (e) => {
-    e.preventDefault()
-    setLat(latChange)
-    setLong(longChange)
-  }
+  const positionTwo = Cartesian3.fromDegrees( -74, 40, 100)
 
 
   return (
-    <div class='text-center'>
-      <Viewer fullscreenButton homeButton={true} terrtainProvider={terrtainProvider}>
-        <CameraFlyTo duration={5} destination={position} once={'once'} />
+      <Viewer full>
+        <Entity position={positionTwo} point={point} />
       </Viewer>
-    </div>
   )
 }
 
