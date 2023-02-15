cfafc91556b474f6797c35bbfe23d5c5ec207be9
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-14 19:13:04 -0600

    location globe works!!

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import {Cartesian3} from 'cesium'
-import { Viewer, Entity} from "resium";
+import { Viewer, Entity, EntityDescription, CameraFlyTo, PointGraphics} from "resium";
 
 
 
-const Globe = ({lat, long, setLat, setLong}) => {
+const Globe = ({lat, long}) => {
   const point = {pixelSize: 10}
+  const globeStyle = {
+    position: "absolute",
+    top: 72,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
 
   const position = Cartesian3.fromDegrees( long, lat, 1000)
   const positionTwo = Cartesian3.fromDegrees( -74, 40, 1000)
 
 
+
+
   return (
-      <Viewer full>
-        <Entity position={positionTwo} point={point} />
-      </Viewer>
+  
+      <div class='w-full h-screen'>
+        <Viewer style={globeStyle} homeButton={true}>
+          <CameraFlyTo duration={5} destination={position}>
+          <Entity>
+            <PointGraphics pixelSize={10} />
+            <EntityDescription>
+              <h1>Test</h1>
+            </EntityDescription>
+          </Entity>
+          </CameraFlyTo>
+        </Viewer>
+      </div>
   )
 }
 
